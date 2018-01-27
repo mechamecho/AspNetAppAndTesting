01efe31eda86524c8da1fc4ae76020f5973dd25c
Livepurposefully <nafissa_92@hotmail.com>
2018-01-27 01:01:11 -0500

    Implement Cancel function

---
+++ GigHub/Models/UserNotification.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GigHub.Models
+{
+    public class UserNotification
+    {
+        [Key]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public int NotificationId { get; set; }
+
+    }
+}
